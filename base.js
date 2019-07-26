4f056685460d37eb4ae3f54cdd2c30e66187ebed
Ken Vermette <vermette@gmail.com>
2019-07-26 16:44:43 -0400

    Post-stylesheet removal

--- base.js
+++ base.js
@@ -5,10 +5,15 @@
  */
 (function($)
 {
+	// Murder the native Freshservice stylesheet. It causes problems.
+	// Because the native stylesheet is listed before his script,
+	// we don't need to wait for the page to load.
+	$('link[href^="/support/theme.css"]').remove();
+	
+	// Hide the loading overlay once loaded.
 	$(window).load(function () {
 		$('#body-loading-overlay').addClass('complete');
 	});
 	
 	
-	$('link[href^="/support/theme.css"]').remove();
 }(jQuery));
